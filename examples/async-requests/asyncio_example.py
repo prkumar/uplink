1dafa2047d710c85d264961a1b4d0bbf6f0d4ca4
P. Raj Kumar <raj.pritvi.kumar@gmail.com>
2017-12-05 18:32:41 -0800

    Update `asyncio_example.py` to work on Python 3.4

--- examples/async-requests/asyncio_example.py
+++ examples/async-requests/asyncio_example.py
@@ -1,34 +1,29 @@
 """
 Example of using Uplink with aiohttp for non-blocking HTTP requests.
-This should work on Python 3.5 and above.
-
-To support Python 3.4, change ``async def`` to ``def``, replace ``await``
-with ``yield from``, and decorate ``get_repo`` with ``asyncio.coroutine``.
+This should work on Python 3.4 and above.
 """
 import asyncio
-import aiohttp
 import uplink
 
 # Local imports
 from github import BASE_URL, GitHub
 
 
-async def get_contributors(full_name):
+@asyncio.coroutine
+def get_contributors(full_name):
     print('Getting GitHub repository `{}`'.format(full_name))
-    response = await gh_async.get_contributors(*full_name.split("/"))
-    json = await response.json()
+    response = yield from gh_async.get_contributors(*full_name.split("/"))
+    json = yield from response.json()
     print('response for {}: {}'.format(full_name, json))
     return json
+
 
 if __name__ == "__main__":
     # This consumer instance uses Requests to make blocking requests.
     gh_sync = GitHub(base_url=BASE_URL)
 
     # This uses an aiohttp, an HTTP client for asyncio.
-    connector = aiohttp.TCPConnector(verify_ssl=False)
-    session = aiohttp.ClientSession(connector=connector)
-    client = uplink.AiohttpClient(session)
-    gh_async = GitHub(base_url=BASE_URL, client=client)
+    gh_async = GitHub(base_url=BASE_URL, client=uplink.AiohttpClient())
 
     # First, let's fetch a list of all public repositories.
     repos = gh_sync.get_repos().json()
