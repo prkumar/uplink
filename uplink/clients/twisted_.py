dd01a268a46a123249cf08efd16ca695a80babe9
P. Raj Kumar <raj.pritvi.kumar@gmail.com>
2017-12-08 17:21:46 -0800

    Update documentation for `TwistedClient`

--- uplink/clients/twisted_.py
+++ uplink/clients/twisted_.py
@@ -1,27 +1,31 @@
 # Third party imports
-
 try:
     from twisted.internet import threads
 except ImportError:
     threads = None
 
 # Local imports
-from uplink.clients import requests_
-from uplink.clients import interfaces
+from uplink.clients import interfaces, register
 
 
 class TwistedClient(interfaces.HttpClientAdapter):
+    """
+    Client that returns :py:class:`twisted.internet.defer.Deferred`
+    responses.
 
-    def __init__(self, client=None):
-        if client is None:
-            client = requests_.RequestsClient()
-        self._requests = client
+    Args:
+        session (:py:class:`requests.Session`, optional): The session
+            that should handle sending requests. If this argument is
+            omitted or set to :py:obj:`None`, a new session will be
+            created.
+    """
+
+    def __init__(self, session=None):
+        if threads is None:
+            raise NotImplementedError("TwistedClient is not installed.")
+        self._requests = register.get_client(session)
 
     def create_request(self):
-        if threads is None:
-            raise NotImplementedError(
-                "TwistedClient is not installed."
-            )
         return Request(self._requests.create_request())
 
 
