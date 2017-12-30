412d5389b16b063807c62b1c620646e6c6162c80
FabioRosado <fabioglrosado@gmail.com>
2017-12-30 08:54:37 +0000

    Add code to execute example from github.py in async-requests folder

--- examples/async-requests/github.py
+++ examples/async-requests/github.py
@@ -14,3 +14,19 @@ class GitHub(uplink.Consumer):
     @uplink.get("/repos/{owner}/{repo}/contributors")
     def get_contributors(self, owner, repo):
         """Lists contributors for the specified repository."""
+
+# Executes the code above
+# The for loop is used for readability, you can just call print(response.json())
+# Uncomment the code you wish to see in action
+
+# Gets all public repositories
+# github = GitHub(BASE_URL)
+# response = github.get_repos()
+# for repos in response.json():
+#     print(repos)
+#
+# # Lists contributors for a repository
+# github = GitHub(BASE_URL)
+# response = github.get_contributors("prkumar", "uplink")
+# for contributors in response.json():
+#     print(contributors)
