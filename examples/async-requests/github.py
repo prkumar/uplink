c085405967e20c527d93b4132058f5aacb690531
FabioRosado <fabioglrosado@gmail.com>
2018-01-09 06:51:53 +0000

    Move execution code of async-request/github.py to if __name__ == __main__

--- examples/async-requests/github.py
+++ examples/async-requests/github.py
@@ -15,18 +15,18 @@ class GitHub(uplink.Consumer):
     def get_contributors(self, owner, repo):
         """Lists contributors for the specified repository."""
 
-# Executes the code above
-# The for loop is used for readability, you can just call print(response.json())
-# Uncomment the code you wish to see in action
+if __name__ == "__main__":
+    # Executes the code above
+    # The for loop is used for readability, you can just call print(response.json())
 
-# Gets all public repositories
-# github = GitHub(BASE_URL)
-# response = github.get_repos()
-# for repos in response.json():
-#     print(repos)
-#
-# # Lists contributors for a repository
-# github = GitHub(BASE_URL)
-# response = github.get_contributors("prkumar", "uplink")
-# for contributors in response.json():
-#     print(contributors)
+    # Gets all public repositories
+    github = GitHub(BASE_URL)
+    response = github.get_repos()
+    for repos in response.json():
+        print(repos)
+
+    # Lists contributors for a repository
+    github = GitHub(BASE_URL)
+    response = github.get_contributors("prkumar", "uplink")
+    for contributors in response.json():
+        print(contributors)
