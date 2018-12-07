f4d5f6bffabadba460d88aac0d478ca618168831
P. Raj Kumar <raj.pritvi.kumar@gmail.com>
2018-12-07 15:17:17 -0800

    DEPLOY: first development version to PyPI

--- verify_version.py
+++ verify_version.py
@@ -17,21 +17,15 @@ def _get_current_version():
     return about.get("__version__", None)
 
 
-def _get_dev_version(build_number):
-    return ".dev" + build_number
-
-
-def build_version(tag, build_number=None):
+def verify_version(tag):
     # Get version defined in package or from the tag
     version = _get_current_version()
-    assert version is not None, "The version in uplink/__about__.py is undefined."
+    assert version is not None, "The version is not defined in uplink/__about__.py."
+    assert tag is not None, "The tag is not defined."
 
-    if not tag:
-        assert build_number is not None
-        version += _get_dev_version(build_number)
-    else:
-        # Make sure the tag version and version in __about__.py are the same
-        assert tag == version, "The tag [%s] does not match the current version in uplink/__about__.py [%s]" % (tag, version)
+    # Make sure the tag version and version in __about__.py are the same
+    if tag is not None:
+        assert tag == ("v" + version), "The tag [%s] does not match the current version in uplink/__about__.py [%s]" % (tag, version)
 
     assert is_canonical(version), "The version string [%s] violates PEP-440"
     return version
@@ -40,10 +34,9 @@ def build_version(tag, build_number=None):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--tag", nargs="?")
-    parser.add_argument("--build-number", required=True)
     args = parser.parse_args()
 
-    print(build_version(args.tag, args.build_number))
+    print(verify_version(args.tag))
 
 
 if __name__ == "__main__":
