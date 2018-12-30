be6cbe4158a4d733d4b48bbee34fbe7653605d42
P. Raj Kumar <raj.pritvi.kumar@gmail.com>
2018-12-30 12:32:56 -0800

    Add raise_on_limit argument to ratelimit

--- tests/integration/test_ratelimit.py
+++ tests/integration/test_ratelimit.py
@@ -1,15 +1,36 @@
+# Third-party imports.
+import pytest
+
 # Local imports.
 import uplink
-from uplink.ratelimit import now
+from uplink.ratelimit import RateLimitExceeded, now
 
 # Constants
 BASE_URL = "https://api.github.com/"
 
 
+class CustomRateLimitException(RuntimeError):
+    pass
+
+
 class GitHub(uplink.Consumer):
-    @uplink.ratelimit(calls=1, period=1)
-    @uplink.get("/users/{user}")
+    @uplink.ratelimit(
+        calls=1, period=10, raise_on_limit=True
+    )  # 1 call every 10 seconds
+    @uplink.get("users/{user}")
     def get_user(self, user):
+        pass
+
+    @uplink.ratelimit(calls=1, period=1, raise_on_limit=False)
+    @uplink.get("repos/{user}/{repo}")
+    def get_repo(self, user, repo):
+        pass
+
+    @uplink.ratelimit(
+        calls=1, period=10, raise_on_limit=CustomRateLimitException
+    )
+    @uplink.get("repos/{user}/{repo}/comments/{comment}")
+    def get_comment(self, user, repo, comment):
         pass
 
 
@@ -21,23 +42,31 @@ def test_limit_exceeded_by_1(mock_client):
     github = GitHub(base_url=BASE_URL, client=mock_client)
 
     # Run
-    start_time = now()
     github.get_user("prkumar")
-    github.get_user("prkumar")
-    elapsed_time = now() - start_time
 
-    # Verify
-    assert elapsed_time >= 1
+    with pytest.raises(RateLimitExceeded):
+        github.get_user("prkumar")
 
 
-def test_exact_limit(mock_client):
+def test_limit_exceeded_by_1_with_custom_exception(mock_client):
     # Setup
     github = GitHub(base_url=BASE_URL, client=mock_client)
 
     # Run
-    start_time = now()
-    github.get_user("prkumar")
-    elapsed_time = now() - start_time
+    github.get_comment("prkumar", "uplink", "1")
 
-    # Verify
-    assert elapsed_time <= 1
+    with pytest.raises(CustomRateLimitException):
+        github.get_comment("prkumar", "uplink", "1")
+
+
+def test_exceeded_limit_wait(mock_client):
+    # Setup
+    github = GitHub(base_url=BASE_URL, client=mock_client)
+
+    # Run
+    start = now()
+    github.get_repo("prkumar", "uplink")
+    github.get_repo("prkumar", "uplink")
+    elapsed = now() - start
+
+    assert elapsed >= 1
--- uplink/ratelimit.py
+++ uplink/ratelimit.py
@@ -6,13 +6,21 @@ import time
 import sys
 
 # Local imports
-from uplink import decorators
+from uplink import decorators, utils
 from uplink.clients.io import RequestTemplate, transitions
 
-__all__ = ["ratelimit"]
+__all__ = ["ratelimit", "RateLimitExceeded"]
 
 # Use monotonic time if available, otherwise fall back to the system clock.
 now = time.monotonic if hasattr(time, "monotonic") else time.time
+
+
+class RateLimitExceeded(RuntimeError):
+    def __init__(self, calls, period):
+        super(RateLimitExceeded, self).__init__(
+            "Exceeded rate limit of [%s] calls every [%s] seconds."
+            % (calls, period)
+        )
 
 
 class Limiter(object):
@@ -43,13 +51,16 @@ class Limiter(object):
 
 
 class RateLimiterTemplate(RequestTemplate):
-    def __init__(self, limiter):
+    def __init__(self, limiter, create_limit_reached_exception):
         self._limiter = limiter
+        self._create_limit_reached_exception = create_limit_reached_exception
 
     def before_request(self, request):
         with self._limiter.check() as ok:
             if ok:
                 return transitions.send(request)
+            elif self._create_limit_reached_exception is not None:
+                raise self._create_limit_reached_exception()
             else:
                 return transitions.sleep(self._limiter.period_remaining)
 
@@ -57,21 +68,41 @@ class RateLimiterTemplate(RequestTemplate):
 # noinspection PyPep8Naming
 class ratelimit(decorators.MethodAnnotation):
     """
-    A decorator that constrains the decorated consumer method or
+    A decorator that constrains a consumer method or an entire
     consumer to making a specified maximum number of requests within a
     defined time period (e.g., 15 calls every 15 minutes).
+
+    By default, when the limit is reached, the client will wait until
+    the current period is over before executing any subsequent
+    requests. If you'd prefer the client to raise an exception when the
+    limit is exceeded, set the ``raise_on_limit`` argument.
 
     Args:
         calls (int): The maximum number of allowed calls that the
             consumer can make within the time period.
         period (float): The duration of each time period in seconds.
+        raise_on_limit (:class:`Exception` or bool): An exception to
+            raise when the client exceeds the rate limit. If
+            :obj:`True`, a :class:`uplink.ratelimit.RateLimitExceeded`
+            exception is raised.
     """
 
-    def __init__(self, calls=15, period=900, clock=now):
+    def __init__(self, calls=15, period=900, raise_on_limit=False, clock=now):
         self._max_calls = max(1, min(sys.maxsize, math.floor(calls)))
         self._period = period
         self._clock = clock
         self._limiter = None
+
+        if utils.is_subclass(raise_on_limit, Exception) or isinstance(
+            raise_on_limit, Exception
+        ):
+            self._create_limit_reached_exception = raise_on_limit
+        elif raise_on_limit:
+            self._create_limit_reached_exception = (
+                self._create_rate_limit_exceeded
+            )
+        else:
+            self._create_limit_reached_exception = None
 
     def _get_limiter_for_request(self, _):
         if self._limiter is None:
@@ -80,4 +111,9 @@ class ratelimit(decorators.MethodAnnotation):
 
     def modify_request(self, request_builder):
         limiter = self._get_limiter_for_request(request_builder)
-        request_builder.add_request_template(RateLimiterTemplate(limiter))
+        request_builder.add_request_template(
+            RateLimiterTemplate(limiter, self._create_limit_reached_exception)
+        )
+
+    def _create_rate_limit_exceeded(self):
+        return RateLimitExceeded(self._max_calls, self._period)
