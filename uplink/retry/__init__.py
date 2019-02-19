e93202a6d9d990a58ecd9b11b9d816c9edb4f5a1
P. Raj Kumar <raj.pritvi.kumar@gmail.com>
2019-02-19 14:26:45 -0800

    Import submodules on `from uplink.retry import *`

--- uplink/retry/__init__.py
+++ uplink/retry/__init__.py
@@ -1,4 +1,5 @@
 from uplink.retry.retry import retry
 from uplink.retry.when import RetryPredicate
+from uplink.retry import when, backoff, stop
 
-__all__ = ["retry", "RetryPredicate"]
+__all__ = ["retry", "RetryPredicate", "when", "backoff", "stop"]
