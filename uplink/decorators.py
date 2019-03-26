a85d23a9e99765204a24fb7694ee883725ad248c
P. Raj Kumar <raj.pritvi.kumar@gmail.com>
2019-03-25 21:59:08 -0700

    Fix unit tests

--- uplink/decorators.py
+++ uplink/decorators.py
@@ -95,7 +95,7 @@ class MethodAnnotation(interfaces.Annotation):
         else:
             return is_consumer_class and not (kwargs or args_[1:])
 
-    def __call__(self, class_or_builder, is_class=False):
+    def __call__(self, class_or_builder, **kwargs):
         if self._is_consumer_class(class_or_builder):
             builders = helpers.get_api_definitions(class_or_builder)
             builders = filter(self._is_relevant_for_builder, builders)
@@ -104,7 +104,9 @@ class MethodAnnotation(interfaces.Annotation):
                 self(b, is_class=True)
                 helpers.set_api_definition(class_or_builder, name, b)
         elif isinstance(class_or_builder, interfaces.RequestDefinitionBuilder):
-            class_or_builder.method_handler_builder.add_annotation(self)
+            class_or_builder.method_handler_builder.add_annotation(
+                self, **kwargs
+            )
         return class_or_builder
 
     def modify_request(self, request_builder):
