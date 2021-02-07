prkumar/uplink

commit 89c43d1b9d65965ab9411b80b343acba7484c93c

Update fallback strategies for converters

uplink/converters/standard.py
@@ -1,6 +1,3 @@
-# Standard library imports
-import json
-
 # Local imports
 from uplink.converters import interfaces, register_default_converter_factory
 
@@ -19,18 +16,6 @@ class Cast(interfaces.Converter):
         return self._converter(value)
 
 
-class RequestBodyConverter(interfaces.Converter):
-    @staticmethod
-    def _default_json_dumper(obj):
-        return obj.__dict__  # pragma: no cover
-
-    def convert(self, value):
-        if isinstance(value, str):
-            return value
-        dumped = json.dumps(value, default=self._default_json_dumper)
-        return json.loads(dumped)
-
-
 class StringConverter(interfaces.Converter):
     def convert(self, value):
         return str(value)
@@ -44,12 +29,13 @@ class StandardConverter(interfaces.Factory):
     converters could handle a particular type.
     """
 
-    def create_response_body_converter(self, type_, *args, **kwargs):
-        if isinstance(type_, interfaces.Converter):
-            return type_
+    @staticmethod
+    def pass_through_converter(type_, *args, **kwargs):
+        return type_
 
-    def create_request_body_converter(self, type_, *args, **kwargs):
-        return Cast(type_, RequestBodyConverter())  # pragma: no cover
+    create_response_body_converter = (
+        create_request_body_converter
+    ) = pass_through_converter
 
     def create_string_converter(self, type_, *args, **kwargs):
         return Cast(type_, StringConverter())  # pragma: no cover

tests/unit/test_converters.py
@@ -37,17 +37,6 @@ class TestCast(object):
         assert return_value == 3
 
 
-class TestRequestBodyConverter(object):
-    def test_convert_str(self):
-        converter_ = standard.RequestBodyConverter()
-        assert converter_.convert("example") == "example"
-
-    def test_convert_obj(self):
-        converter_ = standard.RequestBodyConverter()
-        example = {"hello": "2"}
-        assert converter_.convert(example) == example
-
-
 class TestStringConverter(object):
     def test_convert(self):
         converter_ = standard.StringConverter()
@@ -62,9 +51,6 @@ class TestStandardConverter(object):
         # Run & Verify: Pass-through converters
         converter = factory.create_response_body_converter(converter_mock)
         assert converter is converter_mock
-
-        # Run & Verify: Otherwise, return None
-        assert None is factory.create_response_body_converter("converter")
 
 
 class TestConverterFactoryRegistry(object):

uplink/arguments.py
@@ -179,7 +179,8 @@ class ArgumentAnnotation(interfaces.Annotation):
     def modify_request(self, request_builder, value):
         argument_type, converter_key = self.type, self.converter_key
         converter = request_builder.get_converter(converter_key, argument_type)
-        self._modify_request(request_builder, converter(value))
+        converted_value = converter(value) if converter else value
+        self._modify_request(request_builder, converted_value)
 
 
 class TypedArgument(ArgumentAnnotation):

commit 3653a672ee0703119720d0077bb450649af5459c

Avoid converting for unknown return types
In v0.9.3, the `StandardCoverter.create_response_body_converter` was
changed to a pass-through converter: i.e., the method would simply
return the given type as the response converter. This can cause
unintended behavior in the case when the consumer method has a return
annotation that is not handled by any registered converters. In this
case, the converter chain will invoke the standard coverter's behavior,
and try to convert the runtime response by invoking the annotated
type's constructor. This can cause failures that are difficult to debug
for end users, such as talkpython/100daysofcode-with-python-course#53.

This regression was introduced in 89c43d1b9d (#204).

uplink/clients/io/transitions.py
@@ -47,6 +47,8 @@ def fail(exc_type, exc_val, exc_tb):
     """
     Transitions the execution to fail with a specific error.
 
+    This will prompt the execution of any RequestTemplate.after_exception hooks.
+
     Args:
         exc_type: The exception class.
         exc_val: The exception object.
@@ -62,6 +64,8 @@ def fail(exc_type, exc_val, exc_tb):
 def prepare(request):
     """
     Transitions the execution to prepare the given request.
+
+    This will prompt the execution of any RequestTemplate.before_request.
 
     Args:
         request: The intended request data to be sent.

uplink/converters/standard.py
@@ -29,13 +29,12 @@ class StandardConverter(interfaces.Factory):
     converters could handle a particular type.
     """
 
-    @staticmethod
-    def pass_through_converter(type_, *args, **kwargs):
-        return type_
+    def create_request_body_converter(self, cls, request_definition):
+        return cls
 
-    create_response_body_converter = (
-        create_request_body_converter
-    ) = pass_through_converter
+    def create_response_body_converter(self, cls, *args, **kwargs):
+        if isinstance(cls, interfaces.Converter):
+            return cls
 
     def create_string_converter(self, type_, *args, **kwargs):
         return Cast(type_, StringConverter())  # pragma: no cover

tests/unit/test_converters.py
@@ -44,13 +44,23 @@ class TestStringConverter(object):
 
 
 class TestStandardConverter(object):
-    def test_create_response_body_converter(self, converter_mock):
+    def test_create_response_body_converter_with_converter(
+        self, converter_mock
+    ):
         # Setup
         factory = standard.StandardConverter()
 
         # Run & Verify: Pass-through converters
         converter = factory.create_response_body_converter(converter_mock)
         assert converter is converter_mock
+
+    def test_create_response_body_converter_with_unknown_type(self):
+        # Setup
+        factory = standard.StandardConverter()
+
+        # Run & Verify: does not know how to convert any types
+        converter = factory.create_response_body_converter(dict)
+        assert converter is None
 
 
 class TestConverterFactoryRegistry(object):
@@ -487,19 +497,20 @@ class TestPydanticConverter(object):
     def test_convert_complex_model(self):
         from json import loads
         from datetime import datetime
-        from typing import List
 
         class ComplexModel(pydantic.BaseModel):
             when = datetime.utcnow()  # type: datetime
-            where = 'http://example.com'  # type: pydantic.AnyUrl
-            some = [1]  # type: List[int]
+            where = "http://example.com"  # type: pydantic.AnyUrl
+            some = [1]  # type: typing.List[int]
 
         model = ComplexModel()
         request_body = {}
         expected_result = loads(model.json())
 
         converter = converters.PydanticConverter()
-        request_converter = converter.create_request_body_converter(ComplexModel)
+        request_converter = converter.create_request_body_converter(
+            ComplexModel
+        )
 
         result = request_converter.convert(request_body)
 